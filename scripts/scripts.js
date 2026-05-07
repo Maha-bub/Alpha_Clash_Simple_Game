818b2812ccc5f8dea310aa996fb42a9c61a30b26
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-05-08 00:57:20 +0600

    add onclicked functon for start the game when clicked play now button imidiately hide home page and show the playground screen

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,12 @@
 function play(){
-    console.log('btn clicked by player');
+    // console.log('btn clicked by player');
+    // fist step: when clicked play button then home section hidden and next playgound section is show on the browser
+
+    const homeSection=document.getElementById('home-section');
+    console.log(homeSection);
+    homeSection.classList.add('hidden');
+     const playgroundSection=document.getElementById('play-ground');
+     playgroundSection.classList.remove('hidden');
+
+
 }
